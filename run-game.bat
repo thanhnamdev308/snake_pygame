244ea8776813cc8d06c944543cecd2c1ed8a8b97
thanhnamdev308 <thanhnamdev308@gmail.com>
2023-03-20 23:10:29 +0700

    Add run-game.bat and requirements.txt

--- run-game.bat
+++ run-game.bat
@@ -1,2 +1,18 @@
+for /f "delims=" %%i in ('py -c "import pygame; print(pygame.__version__)"') do set PYGAME_VERSION=%%i
+
+@echo off
+setlocal enabledelayedexpansion
+
+for /f "tokens=1 delims=." %%a in ("%PYGAME_VERSION%") do (
+    set /a major=%%a
+)
+
+if !major! LSS 2 (
+    echo Pygame version does not meet requirements
+    py -m pip install -r requirements.txt
+) else (
+    echo Pygame version meets requirements
+)
+
 cd snake_game
 py main.py
